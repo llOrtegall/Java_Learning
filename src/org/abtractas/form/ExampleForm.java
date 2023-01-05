72c446d486191a81876c53830a30d2d44c84509e
llOrtegall <IvanOrtega_97@hotmail.com>
2023-01-05 11:31:54 -0500

    terminando example class abstractas

--- src/org/abtractas/form/ExampleForm.java
+++ src/org/abtractas/form/ExampleForm.java
@@ -1,9 +1,14 @@
 package org.abtractas.form;
 
+import org.abtractas.ElementoForm;
 import org.abtractas.InputForm;
 import org.abtractas.SelectForm;
 import org.abtractas.TextAreaForm;
 import org.abtractas.select.Option;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class ExampleForm {
@@ -18,17 +23,23 @@ public class ExampleForm {
 
     SelectForm language = new SelectForm("language");
     Option java = new Option("1", "Java");
-    language.addOption(java);
-    language.addOption(new Option("2", "Python"));
-    language.addOption(new Option("3", "JavaScript"));
-    language.addOption(new Option("4", "TypeScript"));
+    language.addOption(java)
+    .addOption(new Option("2", "Python"))
+    .addOption(new Option("3", "JavaScript"))
+    .addOption(new Option("4", "TypeScript"));
 
     username.setValue("john.doe");
     password.setValue("123456");
     email.setValue("email@gmail.com");
     age.setValue("25");
+    exp.setValue("I'm a Java developer");
     java.setSelected(true);
 
+    List<ElementoForm> elementos = Arrays.asList(username, password, email, age, exp, language);
+
+    for(ElementoForm e : elementos){
+      System.out.println(e.drawHtml());
+    }
 
   }
 }
